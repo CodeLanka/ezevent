6f26e4ed73f373336021fb73071a074730795d4d
sudoalgorithm <me@knrmalhotra.com>
2018-10-02 23:32:45 +0400

    Twitter login, crashanalytics added. Removed unwanted code in Login Activity

--- app/src/main/java/org/gdgsrilanka/io/LoginActivity.java
+++ app/src/main/java/org/gdgsrilanka/io/LoginActivity.java
@@ -3,17 +3,25 @@ package org.gdgsrilanka.io;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+
+import com.crashlytics.android.Crashlytics;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
+
+import com.facebook.login.Login;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.google.android.gms.auth.api.Auth;
@@ -28,28 +36,44 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
+import io.fabric.sdk.android.Fabric;
 import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
     public static final int RequestSignInCode = 7;
+    private static final int RequestSignInCodeTwitter = 10;
     public FirebaseAuth firebaseAuth;
     public GoogleApiClient googleApiClient;
     com.google.android.gms.common.SignInButton signInButton;
     ImageView bg;
+    private TwitterLoginButton mTwitterLoginButton;
+    private RelativeLayout mRelativeLayout;
+    private CallbackManager mCallbackManager;
 
-    CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_login);
+
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
 
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relative_layout_main);
+
         signInButton = findViewById(R.id.sign_in_button);
         signInButton = findViewById(R.id.sign_in_button);
+
+        mTwitterLoginButton = (TwitterLoginButton) findViewById(R.id.login_button_twitter);
 
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -61,27 +85,26 @@ public class LoginActivity extends AppCompatActivity {
             finish();
         }
 
-        callbackManager = CallbackManager.Factory.create();
+        mCallbackManager = CallbackManager.Factory.create();
 
-        callbackManager = CallbackManager.Factory.create();
-
-        LoginManager.getInstance().registerCallback(callbackManager,
+        LoginManager.getInstance().registerCallback(mCallbackManager,
                 new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
-                        // App code
+                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
                     }
 
                     @Override
                     public void onCancel() {
-                        // App code
+                        Snackbar.make(mRelativeLayout, "Login With Facebook Canceled", Snackbar.LENGTH_SHORT).show();
                     }
 
                     @Override
                     public void onError(FacebookException exception) {
-                        // App code
+                        Snackbar.make(mRelativeLayout, "Error, Login Failed", Snackbar.LENGTH_SHORT).show();
                     }
                 });
+
         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
@@ -104,6 +127,18 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        mTwitterLoginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Snackbar.make(mRelativeLayout, "Error, Login Failed", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     public void UserSignInMethod(){
@@ -112,9 +147,11 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == RequestSignInCode){
@@ -129,13 +166,17 @@ public class LoginActivity extends AppCompatActivity {
             }
 
         }
+
+        if (resultCode == RequestSignInCodeTwitter){
+            mTwitterLoginButton.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
 
     AccessToken accessToken = AccessToken.getCurrentAccessToken();
     boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
 
-    LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+
 
     public void FirebaseUserAuth(GoogleSignInAccount googleSignInAccount) {
 
